442deced723402214495d51e2e3cd87a49bd1c3b
Harsh-Noise <115484614+Harsh-Noise@users.noreply.github.com>
2025-04-21 18:46:48 -0400

    Update NumberWriter.java
    Fixed bug when total image size was too small to fit the label
    Fixed some formattimg

--- NumberWriter.java
+++ NumberWriter.java
@@ -1,4 +1,4 @@
-package mandelbrotSet;
+package harsh.mandelbrotgenerator;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -16,218 +16,230 @@ public class NumberWriter {
 		for(int x = 0; x < 5; x++) {
 			for(int y = 0; y < 7; y++) {
 				if(number[y][x]) {
-					image.setRGB(x + pointer, y + (7*layer), Color.black.getRGB());
+					image.setRGB(x + pointer, y + layer, Color.black.getRGB());
 				} else {
-					image.setRGB(x + pointer, y + (7*layer), Color.white.getRGB());
+					image.setRGB(x + pointer, y + layer, Color.white.getRGB());
 				}
 			}
 		}
 	}
 	
 	public BufferedImage write(BufferedImage image, String text){
-		boolean[][] number;
-		int pointer = 0;
-		int layer = 0;
-	    for(int x = 0; x < text.length(); x++) {
-	    	//System.out.println("letter # " + x);
-	    	//System.out.println("distance " + x * 5);
-
-	    	//Detects if the text has run out of space, and increments the layer
+            boolean[][] number;
+            int pointer = 0;
+            int layer = 0;
+            //Check once to make sure there is room for the label, and return an unlabled image otherwise.
+            for(int x = 0; x < text.length(); x++) {
 	    	if((pointer + 1)*5 > image.getWidth()) {
-		    	//System.out.println("will be out of bounds");
-	    		layer++;
-	    		pointer = 0;
+                    if((layer + 2)*7 > image.getHeight()){
+                        System.out.println("[???] Not enough space available to label the image. Please disable the label writer.");
+                        return(image);
+                    }
+                    layer++;
+                    pointer = 0;    
 	    	}
-	    	
-	    	//defines all characters able to be written
-	        switch(text.charAt(x)) {
-	        case '1':
-	           	number = new boolean[][]{
-                    {false,false,false,false,false},
-                    {false,true,true,false,false},
-                    {false,false,true,false,false},
-                    {false,false,true,false,false},
-                    {false,false,true,false,false},
-                    {false,true,true,true,false},
-                    {false,false,false,false,false}};
-                printArray(image, number, pointer * 5, layer);
-                break;
-			
-			case '2':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,false,false,true,false},
-					{false,true,true,true,false},
-					{false,true,false,false,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '3':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,false,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '4':
-				number = new boolean[][]{
-                    {false,false,false,false,false},
-                    {false,true,false,true,false},
-                    {false,true,false,true,false},
-                    {false,true,true,true,false},
-                    {false,false,false,true,false},
-                    {false,false,false,true,false},
-                    {false,false,false,false,false}};
-                printArray(image, number, pointer * 5, layer);
-                break;
-			
-			case '5':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,false,false},
-					{false,true,true,true,false},
-					{false,false,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '6':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '7':
-				number = new boolean[][]{
-                    {false,false,false,false,false},
-                    {false,true, true,true,false},
-                    {false,false,false,true,false},
-                    {false,false,true,true,false},
-                    {false,false,false,true,false},
-                    {false,false,false,true,false},
-                    {false,false,false,false,false}};
-                printArray(image, number, pointer * 5, layer);
-                break;
-			
-			case '8':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,true,false},
-					{false,true,true,true,false},
-					{false,true,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '9':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '0':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,true,false},
-					{false,true,false,true,false},
-					{false,true,false,true,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-				
-			case '-':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,false,false,false,false},
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false},
-					{false,false,false,false,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case '.':
-				number = new boolean[][]{
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,true,true,false},
-                    {false,false,true,true,false},
-                    {false,false,false,false,false}};
-                printArray(image, number, pointer * 5, layer);
-                break;
-                
-			case ',':
-				number = new boolean[][]{
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,true,true,false},
-                    {false,false,false,true,false},
-                    {false,false,false,false,false}};
-                printArray(image, number, pointer * 5, layer);
-                break;
-                
-			case 'E':
-				number = new boolean[][]{
-					{false,false,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,false,false},
-					{false,true,true,true,false},
-					{false,true,false,false,false},
-					{false,true,true,true,false},
-					{false,false,false,false,false}};
-				printArray(image, number, pointer * 5, layer);
-				break;
-			
-			case ' ':
-				number = new boolean[][]{
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false},
-                    {false,false,false,false,false}};
-                printArray(image, number, pointer * 5, layer);
-                break;
-			}
+            	//increment the pointer
+	        pointer++;
+            }
+            
+            //Get each character array and place it in the correct spot.
+	    for(int x = 0; x < text.length(); x++) {
+	    	if((pointer + 1)*5 > image.getWidth()) {
+                    if((layer + 2)*7 > image.getHeight()){
+                        System.out.println("[!!!] Label writer ran out of space unexpectedly! Send Harsh Noise your settings and command, and tell him it's borked!");
+                        break;
+                    }
+                    layer++;
+                    pointer = 0;    
+	    	}
+                printArray(image, getCharMatrix(text.charAt(x)), pointer * 5, layer * 7);
 	        
 	        //increment the pointer
 	        pointer++;
-			
-		}
-		return(image);
+            }
+            return(image);
 	}
+        
+        private boolean[][] getCharMatrix(char next) {
+            //defines all characters able to be written
+            boolean[][] number;
+            switch (next){
+                case '1':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, false, false},
+                        {false, false, true, false, false},
+                        {false, false, true, false, false},
+                        {false, false, true, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '2':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, true, true, true, false},
+                        {false, true, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '3':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '4':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, false, true, false},
+                        {false, true, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, false, false, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '5':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '6':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '7':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, false, true, true, false},
+                        {false, false, false, true, false},
+                        {false, false, false, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '8':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, true, false},
+                        {false, true, true, true, false},
+                        {false, true, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+                case '9':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '0':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, true, false},
+                        {false, true, false, true, false},
+                        {false, true, false, true, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '-':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case '.':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, true, true, false, false},
+                        {false, true, true, false, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case ',':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, true, true, false, false},
+                        {false, false, true, false, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case 'E':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, false, false},
+                        {false, true, true, true, false},
+                        {false, true, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                case ' ':
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false},
+                        {false, false, false, false, false}};
+                    return(number);
+
+                default:
+                    number = new boolean[][]{
+                        {false, false, false, false, false},
+                        {false, true, true, true, false},
+                        {false, false, false, true, false},
+                        {false, false, true, true, false},
+                        {false, false, false, false, false},
+                        {false, false, true, false, false},
+                        {false, false, false, false, false}};
+                    return(number);
+            }
+        }
 	
 	public static void main(String[] args) {
 		BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
@@ -236,7 +248,7 @@ public class NumberWriter {
 		double num = 0.0000016;
 		
 		//img = writer.write(img, String.valueOf(num));
-		img = writer.write(img, "E1.2 345, 67890");
+		img = writer.write(img, "E1.2 345, 67890 HELLO WORLD");
 		//System.out.println(String.valueOf(num));
 		File outputfile = new File("txt.png");
 	    try {
